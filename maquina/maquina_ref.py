3efc1523f390438c59127a2a63edbe309ad42a79
Christian-Guz <73039leo@universidaddeleon.edu.mx>
2026-03-31 15:33:19 -0600

    Zodiaco

---
+++ maquina/maquina_ref.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+#DISEÑO
+
+#Ventana
+ventana = tk.Tk()
+ventana.title("Máquina de refrescos")
+ventana.geometry("500x700")
+
+
+
+ventana.mainloop()
